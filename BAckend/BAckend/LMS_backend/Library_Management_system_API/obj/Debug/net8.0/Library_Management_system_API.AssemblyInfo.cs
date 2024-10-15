6c105f895424daf1caeb8d0958e0d46c120acd74
Bruse-Wayne-1021 <ut01261tic2024@gmail.com>
2024-10-15 07:13:04 +0530

    dbset

--- BAckend/BAckend/LMS_backend/Library_Management_system_API/obj/Debug/net8.0/Library_Management_system_API.AssemblyInfo.cs
+++ BAckend/BAckend/LMS_backend/Library_Management_system_API/obj/Debug/net8.0/Library_Management_system_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library_Management_system_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64d613719d875b8d38eddb4d50b9431c9a4448f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d9de031a5e20c3a69af54ca2edce38b5ca57ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library_Management_system_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library_Management_system_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
